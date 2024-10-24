d334aefa4a56a4e71a0f82cb69952a6f003c67c4
Brett Harley Mider <brett_mider@dlsu.edu.ph>
2024-10-24 18:05:51 +0800

    Register: use guidelines and chain for layout

--- app/src/main/java/com/mobdeve/s12/mco/RegisterActivity.kt
+++ app/src/main/java/com/mobdeve/s12/mco/RegisterActivity.kt
@@ -3,7 +3,6 @@ package com.mobdeve.s12.mco
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.constraintlayout.widget.ConstraintLayout
 import com.mobdeve.s12.mco.databinding.ActivityRegisterBinding
 
 
@@ -18,15 +17,6 @@ class RegisterActivity : AppCompatActivity() {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
             finish()
-        }
-
-        // dynamic sizing of the content (white background)
-        val heightPixels = resources.displayMetrics.heightPixels.toFloat()
-        val params = viewBinding.registerClContent.layoutParams as ConstraintLayout.LayoutParams
-        if(heightPixels <= MainActivity.NEXUS_5X_HEIGHT) {
-            params.matchConstraintPercentHeight = 0.95f
-        } else {
-            params.matchConstraintPercentHeight = 0.85f
         }
     }
 }
