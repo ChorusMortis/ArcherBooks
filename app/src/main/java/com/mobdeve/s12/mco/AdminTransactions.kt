880446aedfcd815c05de29d2c7193aee698f572e
Brett Harley Mider <brett_mider@dlsu.edu.ph>
2024-10-27 21:18:21 +0800

    feat: Create admin transactions activity

---
+++ app/src/main/java/com/mobdeve/s12/mco/AdminTransactions.kt
@@ -0,0 +1,13 @@
+package com.mobdeve.s12.mco
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.mobdeve.s12.mco.databinding.ActivityAdminTransactionsBinding
+
+class AdminTransactions : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val viewBinding = ActivityAdminTransactionsBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+    }
+}
