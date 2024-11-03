1e846b9c00862dbbb1ecd892c6b5f275ac421b53
Patrick James Marcellana <patrick_james_marcellana@dlsu.edu.ph>
2024-11-03 15:02:44 +0800

    feat: Add card onClick listener to Favorites cards

--- app/src/main/java/com/mobdeve/s12/mco/FavoritesFavsAdapter.kt
+++ app/src/main/java/com/mobdeve/s12/mco/FavoritesFavsAdapter.kt
@@ -1,17 +1,21 @@
 package com.mobdeve.s12.mco
 
+import android.content.Intent
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.mobdeve.s12.mco.databinding.ItemFavoritesCardLightBinding
 
 class FavoritesFavsAdapter(private val data: ArrayList<BookModel>): RecyclerView.Adapter<FavoritesFavsViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FavoritesFavsViewHolder {
-        // TODO Later: Add on-click listener to the card that will launch an intent going to book details
         // TODO MCO3: Add on-click listener for Favorite button that will add the book to the user's favorites list
 
-        val viewBinding = ItemFavoritesCardLightBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return FavoritesFavsViewHolder(viewBinding)
+        val itemFavsViewBinding = ItemFavoritesCardLightBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val favsViewHolder = FavoritesFavsViewHolder(itemFavsViewBinding)
+        addListenerCard(favsViewHolder, itemFavsViewBinding)
+
+        return favsViewHolder
     }
 
     override fun getItemCount(): Int {
@@ -20,5 +24,20 @@ class FavoritesFavsAdapter(private val data: ArrayList<BookModel>): RecyclerView
 
     override fun onBindViewHolder(holder: FavoritesFavsViewHolder, position: Int) {
         holder.bindData(data[position])
+    }
+
+    private fun addListenerCard(holder : FavoritesFavsViewHolder, itemFavsViewBinding: ItemFavoritesCardLightBinding) {
+        holder.itemView.setOnClickListener(View.OnClickListener {
+            val intent = Intent(holder.itemView.context, BookDetailsActivity::class.java)
+            intent.putExtra(BookDetailsActivity.TITLE_KEY, data[holder.bindingAdapterPosition].title)
+            intent.putExtra(BookDetailsActivity.YEAR_PUBLISHED_KEY, data[holder.bindingAdapterPosition].publishYear)
+            intent.putExtra(BookDetailsActivity.AUTHORS_KEY, data[holder.bindingAdapterPosition].authors.joinToString(", "))
+            intent.putExtra(BookDetailsActivity.COVER_KEY, data[holder.bindingAdapterPosition].coverResource)
+            intent.putExtra(BookDetailsActivity.STATUS_KEY, "Book Available") // TODO MCO3 comes from transaction
+            intent.putExtra(BookDetailsActivity.SHELF_LOCATION_KEY, data[holder.bindingAdapterPosition].shelfLocation)
+            intent.putExtra(BookDetailsActivity.DESCRIPTION_KEY, data[holder.bindingAdapterPosition].description)
+            intent.putExtra(BookDetailsActivity.PAGES_KEY, data[holder.bindingAdapterPosition].pageCount)
+            holder.itemView.context.startActivity(intent)
+        })
     }
 }
