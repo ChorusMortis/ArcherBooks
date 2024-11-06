7007a26fa86a9710520b3f414d605fa8f0b6f247
Brett Harley Mider <brett_mider@dlsu.edu.ph>
2024-11-06 23:06:04 +0800

    Search Results: fix search sorting and filter buttons highlighting in new dialog

--- app/src/main/java/com/mobdeve/s12/mco/SearchResultsFragment.kt
+++ app/src/main/java/com/mobdeve/s12/mco/SearchResultsFragment.kt
@@ -1,21 +1,18 @@
 package com.mobdeve.s12.mco
 
 import android.content.res.ColorStateList
-import android.graphics.Typeface
-import android.graphics.drawable.ColorDrawable
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import androidx.compose.ui.graphics.Color
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.bottomsheet.BottomSheetDialog
-import com.mobdeve.s12.mco.databinding.FragmentSearchBinding
 import com.mobdeve.s12.mco.databinding.ComponentSearchresultsSortDialogBinding
+import com.mobdeve.s12.mco.databinding.FragmentSearchBinding
 
 class SearchResultsFragment : Fragment() {
     companion object {
@@ -25,24 +22,35 @@ class SearchResultsFragment : Fragment() {
     private enum class SortOption {
         RELEVANCE,
         NEWEST,
-        OLDEST,
+    }
+
+    private enum class FilterOption {
+        ALL,
+        TITLE,
+        AUTHOR,
+        SUBJECT,
+        PUBLISHER,
     }
 
     private lateinit var searchResultsBinding : FragmentSearchBinding
-    private lateinit var searchFilterButtons : List<Button>
-    private var activeSearchFilterBtn : Button? = null
 
     private var sortDialogBinding : ComponentSearchresultsSortDialogBinding? = null
+
     private var sortDialogOptionButtons : List<Pair<SortOption, Button>>? = null
     private var activeSortOption : SortOption = SortOption.RELEVANCE
-    private var tempSortOption : SortOption? = null
+    private var tempActiveSortOption : SortOption? = null
+
+    private var searchFilterButtons : List<Pair<FilterOption, Button>>? = null
+    private var activeSearchFilterOption : FilterOption = FilterOption.ALL
+    private var tempActiveSearchFilterOption : FilterOption? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         searchResultsBinding = FragmentSearchBinding.inflate(inflater, container, false)
 
-//        initSearchFilterButtons()
+//        // reset sorting to relevance and filtering to all when search is done
 //        searchResultsBinding.searchresultsIbSearchbtn.setOnClickListener {
 //            activeSortOption = SortOption.RELEVANCE
+//            activeSearchFilterOption = FilterOption.ALL
 //        }
 
         searchResultsBinding.searchBtnFilterSort.setOnClickListener {
@@ -56,71 +64,106 @@ class SearchResultsFragment : Fragment() {
         return searchResultsBinding.root
     }
 
-//    private fun initSearchFilterButtons() {
-//        searchFilterButtons = listOf(
-//            searchResultsBinding.searchresultsBtnFiltermatchall,
-//            searchResultsBinding.searchresultsBtnFiltermatchtitle,
-//            searchResultsBinding.searchresultsBtnFiltermatchauthor,
-//            searchResultsBinding.searchresultsBtnFiltermatchsubject,
-//            searchResultsBinding.searchresultsBtnFiltermatchpublisher
-//        )
-//
-//        activeSearchFilterBtn = searchResultsBinding.searchresultsBtnFiltermatchall
-//        searchFilterButtons.forEach { button ->
-//            button.setOnClickListener {
-//                activeSearchFilterBtn?.let {
-//                    it.backgroundTintList = ColorStateList.valueOf(ContextCompat.getColor(requireActivity(), R.color.search_filter_button))
-//                    button.backgroundTintList = ColorStateList.valueOf(ContextCompat.getColor(requireActivity(), R.color.main_green))
-//                    activeSearchFilterBtn = button
-//                }
-//            }
-//        }
-//    }
-
     private fun showSortResultsDialog() {
         val bottomSheetDialog = BottomSheetDialog(requireActivity(), R.style.BottomSheetDialog)
         sortDialogBinding = ComponentSearchresultsSortDialogBinding.inflate(layoutInflater)
         bottomSheetDialog.setContentView(sortDialogBinding!!.root)
 
+        bottomSheetDialog.setOnDismissListener {
+            sortDialogBinding = null
+
+            sortDialogOptionButtons = null
+            tempActiveSortOption = null
+
+            searchFilterButtons = null
+            tempActiveSearchFilterOption = null
+        }
+
+        sortDialogBinding!!.dialogSearchresultsBtnConfirmbtn.setOnClickListener {
+            // save selected sorting option after user hits confirm button
+            tempActiveSortOption?.let {
+                activeSortOption = it
+            }
+
+            // save selected filter option after user hits confirm button
+            tempActiveSearchFilterOption?.let {
+                activeSearchFilterOption = it
+            }
+
+            bottomSheetDialog.dismiss()
+        }
+
+        // initialize sorting option buttons and highlight the active one
+        initSortOptionButtons()
+
+        // initialize search filter buttons and highlight the active one
+        initSearchFilterButtons()
+
+        bottomSheetDialog.show()
+    }
+
+    private fun initSortOptionButtons() {
         sortDialogOptionButtons = listOf(
             SortOption.RELEVANCE to sortDialogBinding!!.dialogSearchresultsBtnSortRelevance,
             SortOption.NEWEST to sortDialogBinding!!.dialogSearchresultsBtnSortNewest,
-//            SortOption.OLDEST to sortDialogBinding!!.dialogSearchresultsBtnFilteroldest
         )
 
         sortDialogOptionButtons!!.forEach { (option, button) ->
             button.setOnClickListener {
                 // highlight the selected sort option but don't save it yet (user must confirm)
-                tempSortOption = option
+                tempActiveSortOption = option
                 highlightSortOption()
             }
         }
 
-        bottomSheetDialog.setOnDismissListener {
-            sortDialogBinding = null
-            sortDialogOptionButtons = null
-            tempSortOption = null
-        }
-
-        sortDialogBinding!!.dialogSearchresultsBtnConfirmbtn.setOnClickListener {
-            // save selected sorting option after user hits confirm button
-            tempSortOption?.let {
-                activeSortOption = it
-            }
-            bottomSheetDialog.dismiss()
-        }
-
         // highlight last selected sorting option
         highlightSortOption()
+    }
 
-        bottomSheetDialog.show()
+    private fun initSearchFilterButtons() {
+        searchFilterButtons = sortDialogBinding?.let {
+            listOf(
+                FilterOption.ALL to it.dialogSearchresultsBtnFilterAll,
+                FilterOption.TITLE to it.dialogSearchresultsBtnFilterTitle,
+                FilterOption.AUTHOR to it.dialogSearchresultsBtnFilterAuthor,
+                FilterOption.SUBJECT to it.dialogSearchresultsBtnFilterSubject,
+                FilterOption.PUBLISHER to it.dialogSearchresultsBtnFilterPublisher
+            )
+        }
+
+        searchFilterButtons!!.forEach { (option, button) ->
+            button.setOnClickListener {
+                // highlight the selected filter option but don't save it yet (user must confirm)
+                tempActiveSearchFilterOption = option
+                highlightSearchFilterButton()
+            }
+        }
+
+        highlightSearchFilterButton()
     }
 
     private fun highlightSortOption() {
         sortDialogOptionButtons?.forEach { (option, button) ->
             // highlight newly selected sorting option, otherwise highlight default/last chosen sort option
-            val isBold = option == tempSortOption || (tempSortOption == null && option == activeSortOption)
-            button.setTypeface(null, if (isBold) Typeface.BOLD else Typeface.NORMAL)
+            // unhighlight the rest of the sorting options
+
+            val isHighlighted = option == tempActiveSortOption || (tempActiveSortOption == null && option == activeSortOption)
+
+            button.backgroundTintList = if (isHighlighted) ColorStateList.valueOf(ContextCompat.getColor(requireActivity(), R.color.main_green)) else null
+            button.setTextColor(if (isHighlighted) Color.parseColor("#FFFFFF") else ContextCompat.getColor(requireActivity(), R.color.dark_background))
+        }
+    }
+
+    private fun highlightSearchFilterButton() {
+        searchFilterButtons?.forEach { (option, button) ->
+            activeSearchFilterOption.let {
+                // highlight newly selected filter option, otherwise highlight default/last chosen filter option
+                // unhighlight the rest of the filter options
+                val isHighlighted = option == tempActiveSearchFilterOption || (tempActiveSearchFilterOption == null && option == activeSearchFilterOption)
+
+                button.backgroundTintList = if (isHighlighted) ColorStateList.valueOf(ContextCompat.getColor(requireActivity(), R.color.main_green)) else null
+                button.setTextColor(if (isHighlighted) Color.parseColor("#FFFFFF") else ContextCompat.getColor(requireActivity(), R.color.dark_background))
+            }
         }
     }
 }
