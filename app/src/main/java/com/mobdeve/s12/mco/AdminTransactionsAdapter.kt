ccae2cacb50f205d4ec2465f4054ea7b00e736ba
Brett Harley Mider <brett_mider@dlsu.edu.ph>
2024-11-14 19:39:20 +0800

    Admin Transactions: add edit transaction status dialog
    Refactor: create separate drawables for alert and bottom dialogs

--- app/src/main/java/com/mobdeve/s12/mco/AdminTransactionsAdapter.kt
+++ app/src/main/java/com/mobdeve/s12/mco/AdminTransactionsAdapter.kt
@@ -1,14 +1,21 @@
 package com.mobdeve.s12.mco
 
+import android.app.AlertDialog
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.mobdeve.s12.mco.databinding.ComponentDialogAdmintransEditstatusBinding
 import com.mobdeve.s12.mco.databinding.ItemAdminTsCardBinding
 
 class AdminTransactionsAdapter(private val data: ArrayList<TransactionModel>): RecyclerView.Adapter<AdminTransactionsViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AdminTransactionsViewHolder {
         val itemAdminTransBinding = ItemAdminTsCardBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         val adminTransViewHolder = AdminTransactionsViewHolder(itemAdminTransBinding)
+
+        itemAdminTransBinding.itemAdminEditBtn.setOnClickListener {
+            showEditDialog(parent.context)
+        }
 
         return adminTransViewHolder
     }
@@ -19,5 +26,30 @@ class AdminTransactionsAdapter(private val data: ArrayList<TransactionModel>): R
 
     override fun onBindViewHolder(holder: AdminTransactionsViewHolder, position: Int) {
         holder.bindData(data[position])
+    }
+
+    private fun showEditDialog(context: Context) {
+        val editDialogBinding = ComponentDialogAdmintransEditstatusBinding.inflate(LayoutInflater.from(context))
+        val dialog = AlertDialog.Builder(context, R.style.WrapContentDialog)
+            .setView(editDialogBinding.root)
+            .setCancelable(true)
+            .create()
+
+        // make background transparent so dialog floats
+        dialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
+
+        // TODO: change message based on transaction status
+
+        editDialogBinding.dialogAdmintransBtnCancel.setOnClickListener {
+            dialog.dismiss()
+        }
+
+        editDialogBinding.dialogAdmintransBtnConfirm.setOnClickListener {
+            // TODO: Change transaction status after hitting confirm
+            // TODO: Change edit button after book is returned
+            dialog.dismiss()
+        }
+
+        dialog.show()
     }
 }
