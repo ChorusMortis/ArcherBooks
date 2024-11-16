2436f4aeccb2c85d7f095d52028390344f77c43a
Patrick James Marcellana <patrick_james_marcellana@dlsu.edu.ph>
2024-11-17 00:35:30 +0800

    feat: Integrate firebase to gradle builds

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -52,6 +53,8 @@ android {
 
 dependencies {
 
+    implementation(libs.firebase.firestore)
+    implementation(platform(libs.firebase.bom))
     implementation(libs.justifiedtextview)
     implementation(libs.androidx.navigation.fragment.ktx.v2xx)
     implementation(libs.androidx.navigation.ui.ktx.v2xx)
