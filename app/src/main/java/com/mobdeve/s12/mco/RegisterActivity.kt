8e3016b0cead37e9473dd6930d540ad7ca806f2e
Patrick James Marcellana <patrick_james_marcellana@dlsu.edu.ph>
2024-11-17 15:37:04 +0800

    feat: Add front end validation for Registering accounts

--- app/src/main/java/com/mobdeve/s12/mco/RegisterActivity.kt
+++ app/src/main/java/com/mobdeve/s12/mco/RegisterActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.transition.Visibility
 import com.mobdeve.s12.mco.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
@@ -29,14 +30,11 @@ class RegisterActivity : AppCompatActivity() {
     }
 
     private fun addListenerCreateAccountBtn() {
-        // TODO MCO3: Get input, validate if email address is unique, and add account to database
-        // TODO MCO3: Add password and confirm password validation
-        // TODO MCO3: Add input validation (1) empty fields, (2) whitespaces, (3) invalid characters
-        // TODO MCO3: Password validation (1) length, (2) characters
-
         viewBinding.registerBtnRegisterbtn.setOnClickListener(View.OnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+            if(allFieldsValid()) {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }
         })
     }
 
@@ -46,5 +44,60 @@ class RegisterActivity : AppCompatActivity() {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         })
+    }
+
+    private fun allFieldsValid() : Boolean {
+        if(!areAllFieldsFilled()) {
+            viewBinding.registerTvWarning.text = viewBinding.root.context.getString(R.string.warning_incomplete_fields)
+            viewBinding.registerTvWarning.visibility = View.VISIBLE
+        } else if(!isEmailAddValid()) {
+            viewBinding.registerTvWarning.text = viewBinding.root.context.getString(R.string.warning_invalid_email)
+            viewBinding.registerTvWarning.visibility = View.VISIBLE
+        } else if(!isEmailUnique()){
+            viewBinding.registerTvWarning.text = viewBinding.root.context.getString(R.string.warning_email_already_exists)
+            viewBinding.registerTvWarning.visibility = View.VISIBLE
+        } else if(!isPasswordValid()) {
+            viewBinding.registerTvWarning.text = viewBinding.root.context.getString(R.string.warning_short_password)
+            viewBinding.registerTvWarning.visibility = View.VISIBLE
+        } else if(!arePasswordsMatching()) {
+            viewBinding.registerTvWarning.text = viewBinding.root.context.getString(R.string.warning_password_mismatch)
+            viewBinding.registerTvWarning.visibility = View.VISIBLE
+        } else {
+            viewBinding.registerTvWarning.visibility = View.GONE
+            return true
+        }
+
+        return false
+    }
+
+    private fun areAllFieldsFilled() : Boolean {
+        if(viewBinding.registerEtFullname.text.toString() == "" ||
+            viewBinding.registerEtEmail.text.toString() == "" ||
+            viewBinding.registerEtPassword.text.toString() == "" ||
+            viewBinding.registerEtConfirmpassword.text.toString() == "") {
+            return false
+        } else {
+            return true
+        }
+    }
+
+    private fun isEmailAddValid() : Boolean {
+        val emailRegex = Regex("^[\\w-.]+@([\\w-]+\\.)+[\\w-]{2,4}$")
+        return emailRegex.matches(viewBinding.registerEtEmail.text.toString())
+    }
+
+    private fun isEmailUnique() : Boolean {
+        // TODO MCO3: Access database
+        return true
+    }
+
+    private fun isPasswordValid() : Boolean {
+        val strongPasswordRegex = Regex("^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$")
+        val passwordRegex = Regex("^.{8,}$")
+        return passwordRegex.matches(viewBinding.registerEtPassword.text.toString())
+    }
+
+    private fun arePasswordsMatching() : Boolean {
+        return (viewBinding.registerEtPassword.text.toString() == viewBinding.registerEtConfirmpassword.text.toString())
     }
 }
