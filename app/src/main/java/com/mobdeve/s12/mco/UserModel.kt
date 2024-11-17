deb79f1e3cdb057263d81cddfcb1db04b279eab7
Patrick James Marcellana <patrick_james_marcellana@dlsu.edu.ph>
2024-11-17 12:22:13 +0800

    feat: Include email address in UserModel

--- app/src/main/java/com/mobdeve/s12/mco/UserModel.kt
+++ app/src/main/java/com/mobdeve/s12/mco/UserModel.kt
@@ -1,7 +1,25 @@
 package com.mobdeve.s12.mco
 
-class UserModel(
-    val userId: String,
-    val firstName: String,
-    val lastName: String) {
+class UserModel {
+    var userId: String = "-1"
+        private set
+    var firstName: String
+        private set
+    var lastName: String
+        private set
+    var emailAddress: String
+        private set
+
+    constructor(userId: String, firstName: String, lastName: String, emailAddress: String) {
+        this.userId = userId
+        this.firstName = firstName
+        this.lastName = lastName
+        this.emailAddress = emailAddress
+    }
+
+    constructor(firstName: String, lastName: String, emailAddress: String) {
+        this.firstName = firstName
+        this.lastName = lastName
+        this.emailAddress = emailAddress
+    }
 }
