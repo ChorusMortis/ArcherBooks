ba2f22b23a5af87289f8d1fedda2d5cd08f6f8ad
Patrick James Marcellana <patrick_james_marcellana@dlsu.edu.ph>
2024-11-17 12:22:59 +0800

    feat: Include firestore and Firebase Auth in Gradle dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,7 @@ android {
 
 dependencies {
 
+    implementation(libs.google.firebase.auth)
     implementation(libs.firebase.firestore)
     implementation(platform(libs.firebase.bom))
     implementation(libs.justifiedtextview)
@@ -75,6 +76,7 @@ dependencies {
     implementation(libs.androidx.legacy.support.v4)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.firebase.auth)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
