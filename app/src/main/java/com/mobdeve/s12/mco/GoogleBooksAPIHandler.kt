04dc92cd3ed51d5079bb6fe9280a1c4bee3b09f3
Patrick James Marcellana <patrick_james_marcellana@dlsu.edu.ph>
2024-11-21 21:30:28 +0800

    feat: Add getBook() and generateBookObject() methods in GoogleBooksAPIHandler

--- app/src/main/java/com/mobdeve/s12/mco/GoogleBooksAPIHandler.kt
+++ app/src/main/java/com/mobdeve/s12/mco/GoogleBooksAPIHandler.kt
@@ -14,6 +14,89 @@ import kotlin.random.Random
 
 class GoogleBooksAPIHandler {
 
+    suspend fun getBook(bookId: String) : BookModel? {
+        val client = OkHttpClient()
+        var url = "https://www.googleapis.com/books/v1/volumes/$bookId"
+
+        return withContext(Dispatchers.IO) {
+            try {
+                val request = Request.Builder().url(url).build()
+                val response = client.newCall(request).execute()
+                if (response.isSuccessful) {
+                    val json = JSONObject(response.body?.string()!!)
+                    Log.d("GoogleBooksAPIHandler getBook", "Successfully retrieved book id $bookId with title = ${json.getString("id")}")
+
+                    val convertedBook = generateBookObject(json)
+                    Log.d("GoogleBooksAPIHandler getBook", "Successfully converted book id $bookId to BookModel type")
+                    convertedBook
+                } else {
+                    Log.d("GoogleBooksAPIHandler getBook", "Failed to retrieve response" )
+                    null
+                }
+            } catch(e: Exception) {
+                e.printStackTrace()
+                null
+            }
+        }
+    }
+
+    private fun generateBookObject(bookObject : JSONObject) : BookModel {
+        val bookVolumeInfo = bookObject.getJSONObject("volumeInfo")
+
+        // Handle Authors
+        val authors : ArrayList<String> = arrayListOf()
+        val authorsJSONArray = bookVolumeInfo.optJSONArray("authors")
+        if(authorsJSONArray != null) {
+            for(authorIndex in 0 until authorsJSONArray.length()) {
+                authors.add(authorsJSONArray.getString(authorIndex))
+            }
+        } else {
+            authors.add("Unknown Author")
+        }
+
+        // Handle Description
+        var description = bookVolumeInfo.optString("description")
+        if(description.isNullOrEmpty()) {
+            description = "This book does not have any description yet."
+        }
+
+        // Handle Publisher
+        var publisher = bookVolumeInfo.optString("publisher")
+        if(publisher.isNullOrEmpty()) {
+            publisher = "Unknown"
+        }
+
+        // Handle Published Date
+        var publishedDate = bookVolumeInfo.optString("publishedDate")
+        publishedDate = if(!publishedDate.isNullOrEmpty()) {
+            publishedDate.substring(0, 4)
+        } else {
+            "Unknown Date"
+        }
+
+        // Handle Page Count
+        var pageCount = bookVolumeInfo.optInt("pageCount").toString()
+        if(pageCount == "0") {
+            pageCount = "Unknown"
+        }
+
+        // Handle Book Cover
+        var bookCover = "https://books.google.com/books/publisher/content/images/frontcover/${bookObject.getString("id")}?fife=w640-h960&source=gbs_api"
+
+        return BookModel(
+            bookObject.getString("id"),
+            bookVolumeInfo.getString("title"),
+            authors,
+            description,
+            publisher,
+            bookCover,
+            generateRandomShelfLocation(),
+            publishedDate,
+            pageCount,
+            BookModel.HasTransaction.NONE
+        )
+    }
+
     suspend fun getBookDetails(query: String,
                sortOption: SearchResultsFragment.SortOption,
                filterOption: SearchResultsFragment.FilterOption,
