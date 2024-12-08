259589ac5e1b53e301ba4e0ba151cd55626e19d5
Patrick James Marcellana <patrick_james_marcellana@dlsu.edu.ph>
2024-12-09 02:10:13 +0800

    feat: Implement infinite scrolling for admin transactions activity + filter button

--- app/src/main/java/com/mobdeve/s12/mco/AdminTransactionsActivity.kt
+++ app/src/main/java/com/mobdeve/s12/mco/AdminTransactionsActivity.kt
@@ -9,6 +9,7 @@ import android.view.ContextThemeWrapper
 import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
 import android.widget.PopupMenu
 import androidx.appcompat.app.AlertDialog
@@ -17,26 +18,40 @@ import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.firebase.firestore.DocumentSnapshot
+import com.mobdeve.s12.mco.TransactionsFragment.Companion
 import com.mobdeve.s12.mco.databinding.ActivityAdminTransactionsBinding
 import com.mobdeve.s12.mco.databinding.ComponentDialogAdminTransBinding
 import com.mobdeve.s12.mco.databinding.ComponentDialogConfirmlogoutBinding
+import com.mobdeve.s12.mco.databinding.FragmentTransactionsBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class AdminTransactionsActivity : AppCompatActivity() {
     companion object {
         private const val VERTICAL_SPACE = 12
     }
 
-    private enum class FilterOption {
+    enum class FilterOption {
         ALL,
-        TO_PICKUP,
+        FOR_PICKUP,
         TO_RETURN,
         RETURNED,
         CANCELLED,
     }
 
+    private var transactionsObjList : ArrayList<TransactionModel> = arrayListOf()
+    private var displayIncrement = 8L
+    private var isLoading = false
+    private var hasMoreData = true
+    private var lastTransactionRetrieved : DocumentSnapshot? = null
+    private var transactionsCount = 0
+
     private lateinit var adminTransBinding : ActivityAdminTransactionsBinding
     private lateinit var rvRecyclerView: RecyclerView
     private lateinit var rvAdapter: AdminTransactionsAdapter
+    private lateinit var layoutManager: LinearLayoutManager
 
     private var dialogBinding : ComponentDialogAdminTransBinding? = null
 
@@ -56,8 +71,138 @@ class AdminTransactionsActivity : AppCompatActivity() {
         setRVRecyclerView()
         initFilterButton()
 
+        passInitialData()
+        addRVScrollListener()
+
+
         adminTransBinding.adminTransBtnOptions.setOnClickListener {
             showPopupMenu()
+        }
+    }
+
+    private fun passInitialData() {
+        adminTransBinding.adminTransTvEmptyList.visibility = View.GONE
+        setTransactionsCount()
+        CoroutineScope(Dispatchers.Main).launch {
+            lastTransactionRetrieved = null
+            isLoading = true
+            rvAdapter.removeAllTransactions()
+            adminTransBinding.adminTransInitialProgressBar.visibility = View.VISIBLE
+            val firestoreHandler = FirestoreHandler.getInstance(this@AdminTransactionsActivity)
+
+            val returnedObjList = firestoreHandler.getTransactionsForAdmin(this@AdminTransactionsActivity.displayIncrement, activeFilterOption, lastTransactionRetrieved)
+            addDataToAdapter(returnedObjList)
+
+            if(returnedObjList?.first?.size == 0) {
+                adminTransBinding.adminTransTvEmptyList.visibility = View.VISIBLE
+            }
+
+            adminTransBinding.adminTransInitialProgressBar.visibility = View.GONE
+            isLoading = false
+        }
+    }
+
+    private fun setTransactionsCount() {
+        val firestoreHandler = FirestoreHandler.getInstance(this)
+        adminTransBinding.adminTransPbTotal.visibility = View.VISIBLE
+        adminTransBinding.adminTransPbFp.visibility = View.VISIBLE
+        adminTransBinding.adminTransPbTr.visibility = View.VISIBLE
+        adminTransBinding.adminTransPbO.visibility = View.VISIBLE
+        adminTransBinding.adminTransPbReturned.visibility = View.VISIBLE
+
+        CoroutineScope(Dispatchers.Main).launch {
+            val details = firestoreHandler.getAdminTransactionsDetails()
+            val totalActiveCount = details["forPickup"]!! + details["toReturn"]!! + details["overdue"]!!
+            var label = " Transactions"
+            if(totalActiveCount == 1L) {
+                label = " Transaction"
+            }
+
+            adminTransBinding.adminTransTvTotalValue.text = totalActiveCount.toString() + label
+            adminTransBinding.adminTransPbTotal.visibility = View.GONE
+            adminTransBinding.adminTransTvFpValue.text = details["forPickup"]!!.toString()
+            adminTransBinding.adminTransPbFp.visibility = View.GONE
+            adminTransBinding.adminTransTvTrValue.text = details["toReturn"]!!.toString()
+            adminTransBinding.adminTransPbTr.visibility = View.GONE
+            adminTransBinding.adminTransTvOdValue.text = details["overdue"]!!.toString()
+            adminTransBinding.adminTransPbO.visibility = View.GONE
+            adminTransBinding.adminTransTvRValue.text = details["returned"]!!.toString()
+            adminTransBinding.adminTransPbReturned.visibility = View.GONE
+        }
+    }
+
+    private fun addDataToAdapter(returnedObjList: Pair<ArrayList<TransactionModel>, DocumentSnapshot?>?) {
+        if(returnedObjList != null) {
+            transactionsObjList = returnedObjList.first
+            lastTransactionRetrieved = returnedObjList.second
+            transactionsCount = transactionsObjList.size
+
+            Log.d("AdminTransactionsActivity", "Retrieved a total of ${transactionsObjList.size} transactions")
+
+            if(lastTransactionRetrieved == null) {
+                hasMoreData = false
+            }
+
+            rvAdapter.addTransactions(transactionsObjList)
+        }
+    }
+
+    private fun addRVScrollListener() {
+        adminTransBinding.adminTransRv.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+
+                val visibleItemCount = layoutManager.childCount
+                val totalItemCount = layoutManager.itemCount
+                val firstVisibleItemPosition = layoutManager.findFirstVisibleItemPosition()
+
+                if(!isLoading && hasMoreData) {
+                    if(visibleItemCount + firstVisibleItemPosition >= totalItemCount
+                        && firstVisibleItemPosition >= 0
+                        && totalItemCount >= displayIncrement) {
+                        incrementData()
+                    }
+                }
+            }
+        })
+    }
+
+    private fun incrementData() {
+        isLoading = true
+        adminTransBinding.adminTransScrollProgressBar.visibility = View.VISIBLE
+        val layoutParams = adminTransBinding.adminTransRv.layoutParams as ViewGroup.MarginLayoutParams
+        layoutParams.bottomMargin = 180
+
+        val firestoreHandler = FirestoreHandler.getInstance(this)
+
+        CoroutineScope(Dispatchers.Main).launch {
+            val returnedObjList = firestoreHandler.getTransactionsForAdmin(this@AdminTransactionsActivity.displayIncrement, activeFilterOption, lastTransactionRetrieved)
+            addDataToAdapter(returnedObjList)
+            adminTransBinding.adminTransScrollProgressBar.visibility = View.GONE
+            layoutParams.bottomMargin = 0
+            isLoading = false
+        }
+    }
+
+    private fun refreshData() {
+        adminTransBinding.adminTransTvEmptyList.visibility = View.GONE
+        lastTransactionRetrieved = null
+        hasMoreData = true
+        CoroutineScope(Dispatchers.Main).launch {
+            isLoading = true
+            rvAdapter.removeAllTransactions()
+            adminTransBinding.adminTransInitialProgressBar.visibility = View.VISIBLE
+            val firestoreHandler = FirestoreHandler.getInstance(this@AdminTransactionsActivity)
+
+            val returnedObjList = firestoreHandler.getTransactionsForAdmin(this@AdminTransactionsActivity.displayIncrement, activeFilterOption, lastTransactionRetrieved)
+            addDataToAdapter(returnedObjList)
+
+            if(returnedObjList?.first?.size == 0) {
+                adminTransBinding.adminTransTvEmptyList.visibility = View.VISIBLE
+            }
+
+            adminTransBinding.adminTransInitialProgressBar.visibility = View.GONE
+            isLoading = false
         }
     }
 
@@ -83,9 +228,10 @@ class AdminTransactionsActivity : AppCompatActivity() {
 
     private fun setRVRecyclerView() {
         rvRecyclerView = adminTransBinding.adminTransRv
-        rvAdapter = AdminTransactionsAdapter(ArrayList(transactions))
+        rvAdapter = AdminTransactionsAdapter(arrayListOf())
         rvRecyclerView.adapter = rvAdapter
-        rvRecyclerView.layoutManager = LinearLayoutManager(this)
+        layoutManager = LinearLayoutManager(this)
+        rvRecyclerView.layoutManager = layoutManager
         rvRecyclerView.addItemDecoration(MarginItemDecoration(resources.displayMetrics, VERTICAL_SPACE))
     }
 
@@ -149,6 +295,8 @@ class AdminTransactionsActivity : AppCompatActivity() {
                 activeFilterOption = it
             }
 
+            refreshData()
+
             bottomSheetDialog.dismiss()
         }
 
@@ -162,7 +310,7 @@ class AdminTransactionsActivity : AppCompatActivity() {
         filterButtons = dialogBinding?.let {
             listOf(
                 FilterOption.ALL to it.dialogAtBtnFilterAll,
-                FilterOption.TO_PICKUP to it.dialogAtBtnFilterForpickup,
+                FilterOption.FOR_PICKUP to it.dialogAtBtnFilterForpickup,
                 FilterOption.TO_RETURN to it.dialogAtBtnFilterToreturn,
                 FilterOption.RETURNED to it.dialogAtBtnFilterReturned,
                 FilterOption.CANCELLED to it.dialogAtBtnFilterCancelled
